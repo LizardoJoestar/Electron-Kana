d04a0ceba2a9a36c11c3a0ab0e19ce0476124aff
LizardoJoestar <khasdragon@tutanota.com>
2022-05-20 11:03:20 -0700

    Added variables

--- js/practice_words.js
+++ js/practice_words.js
@@ -1,3 +1,19 @@
 import { words } from "./syllabeValues";
 
 var input = localStorage.getItem('input');
+
+// Tracking variables
+var check = false;
+var successes = 0;
+var fails = 0;
+var successPercent = 0;
+var counter = 1; // to avoid dividing by 0
+
+// Maximum number of questions
+let maxQuestions = 10;
+
+// Set up tracking variables-DOM id selection
+let counterID = document.getElementById('counter');
+let successesID = document.getElementById('successes');
+let failsID = document.getElementById('fails');
+let successPercentID = document.getElementById('success-percent');
